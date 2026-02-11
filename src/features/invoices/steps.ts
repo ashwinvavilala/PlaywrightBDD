7429a6ca9fa1f6eb3548b9917b9e67d3216282df
Ashwin Vavilala <ashwinvavilala@gmail.com>
2026-02-11 15:45:38 +0000

    test steps updated

--- src/features/invoices/steps.ts
+++ src/features/invoices/steps.ts
@@ -1,30 +1,36 @@
 import { Given, When, Then } from "../../../bdd-setup";
-import { expect } from "@playwright/test";
+import { expect, Page } from "@playwright/test";
 import { InvoicesPage } from "../../page-objects/invoices-page";
 let invoiceAmount: string;
 let totalInvoicesSum: number;
 
-Given("I am on the Invoices page", async ({ page }) => {
+Given("I am on the Invoices page", async ({ page }: { page: Page }) => {
   const invoicesPage = new InvoicesPage(page);
   await invoicesPage.visit();
 });
 
-When("I look up the amount for invoice {string}", async ({ page }, invoiceNumber: string) => {
-  const invoicesPage = new InvoicesPage(page);
-  invoiceAmount = await invoicesPage.getInvoiceAmount(invoiceNumber);
-});
+When(
+  "I look up the amount for invoice {string}",
+  async ({ page }: { page: Page }, invoiceNumber: string) => {
+    const invoicesPage = new InvoicesPage(page);
+    invoiceAmount = await invoicesPage.getInvoiceAmount(invoiceNumber);
+  }
+);
 
-Then("the amount should be {string}", async ({}, expectedInvoiceAmount: string) => {
-  await expect(invoiceAmount).toEqual(expectedInvoiceAmount);
-});
-
-When("I sum all the invoice amounts in the table", async ({ page }) => {
+When("I sum all the invoice amounts in the table", async ({ page }: { page: Page }) => {
   const invoicesPage = new InvoicesPage(page);
   totalInvoicesSum = await invoicesPage.sumAllInvoicesAmount();
 });
 
-Then("the total invoices sum should match the amount in the summary row", async ({ page }) => {
+When("I sum all the invoice amounts in the table", async ({ page }: { page: Page }) => {
   const invoicesPage = new InvoicesPage(page);
-  const invoicesSummaryAmount: number = await invoicesPage.getInvoicesSummaryAmount();
-  await expect(totalInvoicesSum).toEqual(invoicesSummaryAmount);
+  totalInvoicesSum = await invoicesPage.sumAllInvoicesAmount();
 });
+
+Then("the total invoices sum should match the amount in the summary row",
+  async ({ page }: { page: Page }) => {
+    const invoicesPage = new InvoicesPage(page);
+    const invoicesSummaryAmount = await invoicesPage.getInvoicesSummaryAmount();
+    await expect(totalInvoicesSum).toEqual(invoicesSummaryAmount);
+  }
+);
